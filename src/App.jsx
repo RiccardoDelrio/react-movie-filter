037ad8a55926bdc79689afde19e6a82fa83ffcfb
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-27 16:32:43 +0100

    Refactor film selection logic and update UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-const films = [
+import { useState, useEffect } from 'react'
+const filmsarray = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -10,20 +10,51 @@ const films = [
 
 
 function App() {
+  const [films, setFilms] = useState(filmsarray)
+  const [selectedGenre, setSelectedGenre] = useState("")
+  console.log(selectedGenre);
+  useEffect(() => {
+    if (selectedGenre) {
+      const mappedFilms = filmsarray.filter((thisfilm,) =>
+        selectedGenre === thisfilm.genre
+      )
+      console.log(mappedFilms);
+      setFilms(mappedFilms)
+    } else {
+      setFilms(filmsarray)
+    }
+  }, [selectedGenre])
 
   return (
     <>
-      <div className="container">
-        <div className="row align-content-center">
-          <div className="col-6">
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-3">
 
-            <select className="form-select" aria-label="Default select example">
+            <select
+              value={selectedGenre}
+              onChange={(e) => setSelectedGenre(e.target.value)}
+              className="form-select"
+              aria-label="Default select example">
               <option value="Scegli un genere"   >Scegli un genere</option>
-              {films.map((thisfilm, index) => <option key={index} value={thisfilm.genre} >{thisfilm.genre}</option>)} {/* GENERAZIONE AUTOMATICA DELLE OPZIONI */}
+              {filmsarray.map((thisfilm, index) =>
+                <option key={index}   >{thisfilm.genre}</option>)} {/* GENERAZIONE AUTOMATICA DELLE OPZIONI */}
             </select>
           </div>
         </div>
-      </div>
+        <div className="row">
+          <div className="col-12 mt-4">
+            <ul className="list-group">
+              {films.map((thisfilm, index) =>
+                <li key={index} className="list-group-item">{thisfilm.title}</li>
+              )}
+
+            </ul>
+
+          </div>
+
+        </div>
+      </div >
 
     </>
   )
